62fbf131da635a9485bec1342c2a080e3c90c579
jooohyunpark <jhp527@nyu.edu>
2022-04-07 21:00:09 -0400

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <Tutorial/>
+  <div class="page">
+    <div class="container">
+      <div class="row">
+        <div class="col"><h1>Website of You</h1></div>
+      </div>
+
+      <div class="row">
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+        <div class="col">Column</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
