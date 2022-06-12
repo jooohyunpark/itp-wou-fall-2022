36a7bd2e2cea0c874b519668ce48afd24615f469
jooohyunpark <jhp527@nyu.edu>
2022-06-12 19:45:36 -0400

    add home page

--- week1/components/Nav.vue
+++ week1/components/Nav.vue
@@ -4,6 +4,7 @@
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/life-cycle">Life cycle</NuxtLink>
     <NuxtLink to="/directives">Directives</NuxtLink>
+    <NuxtLink to="/homepage">Home page example</NuxtLink>
   </nav>
 </template>
 <script>
---
+++ week1/pages/homepage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="homepage">
+    <div class="works">
+      <div class="work" v-for="(work, i) of works" :key="i">
+        <img :src="require(`/assets/img/${work.src}`)" :alt="work.title" />
+        <h2>{{ work.title }}</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      works: [
+        { title: "Title 1", src: "img-1.jpg" },
+        { title: "Title 2", src: "img-2.jpg" },
+        { title: "Title 3", src: "img-3.jpg" },
+        { title: "Title 4", src: "img-4.jpg" },
+        { title: "Title 5", src: "img-5.jpg" },
+        { title: "Title 6", src: "img-6.jpg" },
+        { title: "Title 7", src: "img-7.jpg" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.homepage {
+  max-width: 1280px;
+  margin: 0 auto;
+}
+
+.works {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.work {
+  width: 33.33%;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.work img {
+  width: 100%;
+}
+</style>
