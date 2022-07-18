133a867fa7cf38b8d286d558f65a6d7d01c57f09
jooohyunpark <jhp527@nyu.edu>
2022-07-18 20:32:11 +0900

    update

--- components/Objective.vue
+++ components/Objective.vue
@@ -3,7 +3,7 @@
     <h2>Course Objectives</h2>
     <p>At the completion of this course, the students will:</p>
     <ol>
-      <li>Be comfortable designing visuals/UX for static web application</li>
+      <li>Be comfortable designing UI/UX for static web application</li>
       <li>Learn modular programming patterns in javascript using Vue.js</li>
       <li>Build and deploy a custom portfolio website (or equivalent)</li>
     </ol>
