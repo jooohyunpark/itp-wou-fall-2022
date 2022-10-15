30ecaf629331ef4625188a5003a74086a01f9395
jooohyunpark <jhp527@nyu.edu>
2022-10-15 01:26:15 -0400

    add source pgae

--- p5-three/pages/p5-embed.vue
+++ p5-three/pages/p5-embed.vue
@@ -3,7 +3,10 @@
     <div class="container">
       <div class="row">
         <h4>p5 embed</h4>
-        <p>Instead of writing code in vue, just embedding in iframe.</p>
+        <p>
+          Instead of writing code in vue, just embedding in iframe. Source page
+          is <a href="https://jooohyunpark.github.io/tropical/">here</a>.
+        </p>
         <iframe src="https://jooohyunpark.github.io/tropical/"></iframe>
       </div>
     </div>
